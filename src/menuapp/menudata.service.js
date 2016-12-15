168f5503b13d2845fe43e6339e0a9f567f340031
W10\Ruben <rubenleis@gmail.com>
2016-12-15 16:18:38 +0100

    Categories controller works

--- src/menuapp/menudata.service.js
+++ src/menuapp/menudata.service.js
@@ -1,41 +1,58 @@
 (function () {
 'use strict';
 
-angular.module('Data')
+angular.module('MenuApp')
 .service('MenuDataService', MenuDataService);
 
 /**
 * Service to retrieve menu items
 */
-MenuDataService.$inject = ['$http', 'ApiBasePath'];
-function MenuDataService($http, ApiBasePath) {
+MenuDataService.$inject = ['$http'];
+function MenuDataService($http) {
   var service = this;
 
   /**
   * Get list item that match to searchTerm
+  * Promises and http == >http://stackoverflow.com/questions/12505760/processing-http-response-in-service
   */
   //FIXME: URL in constants
   service.getAllCategories = function () {
 	console.log("MenuDataService", "getAllCategories");
-	var response = $http({
-      method: "GET",
-      url: ("https://davids-restaurant.herokuapp.com/categories.json")
-    });
-	
-	return response;
+	// $http returns a promise, which has a then function, which also returns a promise
+	var promise = $http({
+		 // The then function here is an opportunity to modify the response
+		method: "GET",
+		url: ('https://davids-restaurant.herokuapp.com/categories.json')
+	})
+	.then(function (response) {
+		// Update the response ?
+		console.log(response);
+		 // The return value gets picked up by the then in the controller.
+		return response.data;
+	});
+	// Return the promise to the controller
+	return promise;
   };
 
 	
   service.getItemsForCategory = function(categoryShortName) {
-	var response = $http({
-      method: "GET",
-      url: ("https://davids-restaurant.herokuapp.com/menu_items.json"),
-	  params: {
-        category: categoryShortName
-      }
-	  
-    });
-	return response;  
+	// $http returns a promise, which has a then function, which also returns a promise
+	var promise = $http({
+		 // The then function here is an opportunity to modify the response
+		method: "GET",
+		url: ('https://davids-restaurant.herokuapp.com/menu_items.json'),
+		params: {
+			category: categoryShortName
+		}
+	})
+	.then(function (response) {
+		// Update the response ?
+		console.log(response);
+		 // The return value gets picked up by the then in the controller.
+		return response.data;
+	});
+	// Return the promise to the controller
+	return promise;
   };
 }
 
