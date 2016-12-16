afbd59aba1b06db766c5208a7c3a5600dc59896f
W10\Ruben <rubenleis@gmail.com>
2016-12-16 10:38:50 +0100

    Release 1.0.0

--- src/menuapp/menudata.service.js
+++ src/menuapp/menudata.service.js
@@ -9,8 +9,8 @@ angular.module('MenuApp')
 * Promises and http == >http://stackoverflow.com/questions/12505760/processing-http-response-in-service
 *
 */
-MenuDataService.$inject = ['$http'];
-function MenuDataService($http) {
+MenuDataService.$inject = ['$http','ApiBasePath'];
+function MenuDataService($http, ApiBasePath) {
   var service = this;
 
   /**
@@ -23,11 +23,11 @@ function MenuDataService($http) {
 	var promise = $http({
 		 // The then function here is an opportunity to modify the response
 		method: "GET",
-		url: ('https://davids-restaurant.herokuapp.com/categories.json')
+		url: (ApiBasePath + "/categories.json")
 	})
 	.then(function (response) {
 		// Update the response ?
-		//console.log("getAllCategories response:");
+		console.log("getAllCategories response:");
 		//console.log(response);
 		 // The return value gets picked up by the then in the controller.
 		return response.data;
@@ -44,7 +44,7 @@ function MenuDataService($http) {
 	// $http returns a promise, which has a then function, which also returns a promise
 	var promise = $http({
 		 // The then function here is an opportunity to modify the response
-		url: ('https://davids-restaurant.herokuapp.com/menu_items.json'),
+		url: (ApiBasePath + "/menu_items.json"),
 		method: "GET",
 		params: {
 			category: categoryShortName
